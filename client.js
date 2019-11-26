a0a00ab30dbc5951e01e0d153f3786bdf70ca6ef
Corbin Smith <corbintheintern@gmail.com>
2019-11-25 19:14:18 -0600

    Fixed some program-breaking bugs, probably made a few more. We'll see.

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ var can, ctx;
 var CAN_W = 960;
 var CAN_H = 640;
 var FPS = 60;
+var smoothingLoops = 5;
 
 var GameStates = {
     'MainMenu': 0,
@@ -22,43 +23,47 @@ function init() {
     currentGameState = GameStates.MainMenu;
 
     worldMap = instantiateWorldMap(60, 40);
-    
+
     setInterval(_LOOP, 1000 / FPS);
 }
 
 function instantiateWorldMap(xSize, ySize) {
-    let map = [];
-    for(let i = 0; i < size; i++){
+    let map = new Array(ySize);
+    for (let i = 0; i < xSize; i++) {
         map[i] = [];
     }
-    
-    for(let x = 0; x < xSize; x++){
-        for(let y = 0; y < ySize; y++){
+
+    for (let x = 0; x < xSize; x++) {
+        for (let y = 0; y < ySize; y++) {
             let rand = Math.random();
-            if(rand > 0.5 && x != 0 && y != 0 && x != size-1 && y!= size-1) map[x][y] = 1;
+            if (rand > 0.5 && x != 0 && y != 0 && x != xSize - 1 && y != ySize - 1) map[x][y] = 1;
             else map[x][y] = 0;
         }
     }
     console.log(map);
-    
-    for(let k = 0; k < smoothingLoops; k++){
+
+    for (let k = 0; k < smoothingLoops; k++) {
         map = smoothMap(map);
     }
-    
+
     return map;
 }
 
 function smoothMap(map) {
-    
+
     let oldMap = map;
     let newMap = oldMap;
-    
-    for(let x = 0; x < oldMap.length) for(let y = 0; y < oldMap[x].length; y++){
-        if(x > 0 && x < oldMap[x].length && y > 0 && y < oldMap.length){
-            let neighborcount = 0;
-            for(let i = -1; i <= 1; i++) for(let j = -1; j <= 1; j++){
-                if(!(i == 0 && j == 0) && oldMap[x+i][y+j] != 0){
-                    
+
+    for (let x = 0; x < oldMap.length; x++) {
+        for (let y = 0; y < oldMap[x].length; y++) {
+            if (x > 0 && x < oldMap[x].length && y > 0 && y < oldMap.length) {
+                let neighborcount = 0;
+                for (let i = -1; i <= 1; i++) {
+                    for (let j = -1; j <= 1; j++) {
+                        if (!(i == 0 && j == 0) && oldMap[x + i][y + j] != 0) {
+
+                        }
+                    }
                 }
             }
         }
@@ -78,9 +83,9 @@ function draw() {
         case GameStates.WorldMap:
             drawGroundTiles();
             break;
-        case GameStates.Dungeon
-            m = "Dungeon";
-            break;
+        case GameStates.Dungeon:
+        m = "Dungeon";
+        break;
         default:
             m = "Error.";
             break;
@@ -90,10 +95,10 @@ function draw() {
 }
 
 function drawGroundTiles() {
-    for(let i = 0; i < worldMap.length; i++){
-        for(let j = 0; j < worldMap[0].length; j++){
-            if(worldMap[i][j] == 0) {
-                
+    for (let i = 0; i < worldMap.length; i++) {
+        for (let j = 0; j < worldMap[0].length; j++) {
+            if (worldMap[i][j] == 0) {
+
             }
         }
     }
