9b4f9edbf4e7c64130eb73d0b48daf23715779b2
Corbin Smith <corbintheintern@gmail.com>
2019-11-25 19:29:16 -0600

    Moved map generation to its own helper file.

---
+++ helpers/worldgen.js
@@ -0,0 +1,41 @@
+function instantiateWorldMap(xSize, ySize) {
+    let map = new Array(ySize);
+    for (let i = 0; i < xSize; i++) {
+        map[i] = [];
+    }
+
+    for (let x = 0; x < xSize; x++) {
+        for (let y = 0; y < ySize; y++) {
+            let rand = Math.random();
+            if (rand > 0.5 && x != 0 && y != 0 && x != xSize - 1 && y != ySize - 1) map[x][y] = 1;
+            else map[x][y] = 0;
+        }
+    }
+    console.log(map);
+
+    /*for (let k = 0; k < smoothingLoops; k++) {
+        map = smoothMap(map);
+    }*/
+
+    return map;
+}
+
+function smoothMap(map) {
+    let oldMap = map;
+    let newMap = oldMap;
+
+    for (let x = 0; x < oldMap.length; x++) {
+        for (let y = 0; y < oldMap[x].length; y++) {
+            if (x > 0 && x < oldMap[x].length && y > 0 && y < oldMap.length) {
+                let neighborcount = 0;
+                for (let i = -1; i <= 1; i++) {
+                    for (let j = -1; j <= 1; j++) {
+                        if (!(i == 0 && j == 0) && oldMap[x + i][y + j] != 0) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
